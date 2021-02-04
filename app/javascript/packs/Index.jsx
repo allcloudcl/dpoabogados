9c86edfd5850761ac55595ea5288823d7adea7f6
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-02-03 23:46:41 -0400

    Move dashboard.css out of js and into the views

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 // import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import '../../assets/stylesheets/dashboard.scss';
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
