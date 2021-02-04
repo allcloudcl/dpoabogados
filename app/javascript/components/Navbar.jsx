00c4e1bbb02667deb14e59e37d50357ae3ec0aba
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-02-04 00:47:58 -0400

    Add burger menu

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import LogOut from "./Users/LogOut";
 
 class Navbar extends React.Component {
     render() {
         return (
             <header className="navbar navbar-dark bg-dark sticky-top flex-md-nowrap p-0 shadow">
-                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">ChileDeudas</a>
-                <ul className="navbar-nav px-3">
-                    <li className="nav-item text-nowrap">
-                        <LogOut />
-                    </li>
-                </ul>
+              <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">ChileDeudas</a>
+              <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
             </header>
         )
     }
--- app/javascript/components/Sidebar.jsx
+++ app/javascript/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import LogOut from './Users/LogOut';
 
 class Sidebar extends React.Component {
 
@@ -9,30 +10,36 @@ class Sidebar extends React.Component {
 
     render() {
         return (
-            <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                <div className="position-sticky pt-3">
+            <div className="container-fluid">
+              <div className="row">
+                <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                  <div className="position-sticky pt-3">
                     <ul className="nav flex-column">
-                        <li className="nav-item">
-                            <NavLink to="/" exact className="nav-link">
-                                <span data-feather="home"></span>
-                                Dashboard
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/contracts" className="nav-link">
-                                <span data-feather="file"></span>
-                                Contracts
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/users" className="nav-link">
-                                <span data-feather="users"></span>
-                                Users
-                            </NavLink>
-                        </li>
+                      <li className="nav-item">
+                        <NavLink to="/" exact className="nav-link">
+                          <span data-feather="home"></span>
+                          Dashboard
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink to="/contracts" className="nav-link">
+                          <span data-feather="file"></span>
+                          Contracts
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink to="/users" className="nav-link">
+                          <span data-feather="users"></span>
+                          Users
+                        </NavLink>
+                      </li>
+                      <hr />
+                      <LogOut />
                     </ul>
-                </div>
-            </nav>
+                  </div>
+                </nav>
+              </div>
+            </div>
         )
     }
 }
