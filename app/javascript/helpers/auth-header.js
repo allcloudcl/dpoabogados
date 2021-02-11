12cdab9fe04b74af5f0ad7e6a9d43e81fd63d0a3
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-02-11 09:40:20 -0400

    Add Redux skel
    From: https://bezkoder.com/react-redux-jwt-auth/

---
+++ app/javascript/helpers/auth-header.js
@@ -0,0 +1,12 @@
+export function authHeader() {
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.authentication_token) {
+        return {
+            'X-User-Token': user.authentication_token,
+            'X-User-Email': user.email
+        };
+    } else {
+        return {};
+    }
+}
