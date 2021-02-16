d06959809db7b79c28e27f0f889faea13d847e42
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-02-16 03:11:24 -0400

    Fix scroll

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div className="d-flex p-3 mx-auto flex-row align-items-center vh-100">
+  <div className="d-flex p-5 mx-auto flex-row align-items-center home">
     <div className="col-md-6">
-        <h1 className="display-5">Software DPO ChileDeudas<br /> al Servicio de sus Clientes</h1>
+      <h1 className="display-5">
+        Software DPO ChileDeudas
+        <br />
+        al Servicio de sus Clientes
+      </h1>
     </div>
     <div className="col-md-6">
       <p className="lead">
