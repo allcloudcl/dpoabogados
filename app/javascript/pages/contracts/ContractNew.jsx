76cfd1aef664b3ea531a5f628d00b26f82b5199b
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-02-15 22:55:08 -0400

    Improve ContractNew field

--- app/javascript/pages/contracts/ContractNew.jsx
+++ app/javascript/pages/contracts/ContractNew.jsx
@@ -52,7 +52,8 @@ class ContractNew extends React.Component {
         this.setState(prevState => {
             let contract = Object.assign({}, prevState.contract);
             let value_fee = (contract.amount - contract.payment)/contract.dues;
-            contract['value_fee'] = value_fee;
+            let round_value_fee = Number.parseFloat(value_fee).toFixed(2);
+            contract['value_fee'] = round_value_fee;
             return { contract };
         })
     }
@@ -78,22 +79,24 @@ class ContractNew extends React.Component {
 
         return (
             <>
-              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2  border-bottom">
                 <h1 className="h2">Nuevo Contrato</h1>
               </div>
-              <form onSubmit={this.doCreateContract}>
-                <div className="mb-3">
+              <form onSubmit={this.doCreateContract} className="row g-3">
+                <div>
                   <label htmlFor="description" className="form-label">Descripción</label>
-                      <textarea
-                          name="description"
-                          className="form-control"
-                          rows="2"
-                          placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                          onChange={this.onChange}>
-                      </textarea>
+                  <textarea
+                    name="description"
+                    className="form-control"
+                    rows="2"
+                    placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    onChange={this.onChange}
+                    required
+                  >
+                  </textarea>
                 </div>
 
-                <div className="mb-3">
+                <div>
                   <label htmlFor="kind" className="form-label">Tipo de Contrato</label>
                   <select name="kind" className="form-select" value={this.state.contract.kind} onChange={this.onChange}>
                     <option value="deuda">Deuda</option>
@@ -101,112 +104,126 @@ class ContractNew extends React.Component {
                   </select>
                 </div>
 
-                <div className="mb-3">
+                <div>
                   <label htmlFor="user_id" className="form-label">Usuario</label>
                   <select name="user_id" className="form-select" onChange={this.onChange}>
                     {optionsUsers}
                   </select>
                 </div>
 
-                <div className="mb-3">
+                <div>
                   <label htmlFor="creditor" className="form-label">Acreedor</label>
-                      <input
-                          name="creditor"
-                          className="form-control"
-                          placeholder="Lorem ipsum"
-                          onChange={this.onChange}>
-                      </input>
+                  <input
+                    name="creditor"
+                    className="form-control"
+                    placeholder="Lorem ipsum"
+                    onChange={this.onChange}
+                    required
+                  >
+                  </input>
                 </div>
 
-                <div className="mb-3">
+                <div className=" col-md-6">
                   <label htmlFor="amount" className="form-label">Monto</label>
-                      <input
-                          type="number"
-                          name="amount"
-                          className="form-control"
-                          step="any"
-                          placeholder="10.000,00"
-                          min="0"
-                          value={this.state.contract.amount}
-                          onChange={this.updateValueFee}
-                      >
-                      </input>
+                  <input
+                    type="number"
+                    name="amount"
+                    className="form-control"
+                    step="0.01"
+                    placeholder="10.000,00"
+                    min="0"
+                    value={this.state.contract.amount}
+                    onChange={this.updateValueFee}
+                    required
+                  >
+                  </input>
                 </div>
 
-                <div className="mb-3">
-                  <label htmlFor="payment" className="form-label">Pago por Adelantado</label>
-                      <input
-                          type="number"
-                          name="payment"
-                          className="form-control"
-                          step="any"
-                          placeholder="10.000,00"
-                          min="0"
-                          value={this.state.contract.payment}
-                          onChange={this.updateValueFee}
-                      >
-                      </input>
+                <div className=" col-md-6">
+                  <label htmlFor="payment" className="form-label">Pago Adelantado</label>
+                  <input
+                    type="number"
+                    name="payment"
+                    className="form-control"
+                    step="0.01"
+                    placeholder="10.000,00"
+                    min="0"
+                    value={this.state.contract.payment}
+                    onChange={this.updateValueFee}
+                    required
+                  >
+                  </input>
                 </div>
 
-                <div className="mb-3">
+                <div className="col-md-6">
                   <label htmlFor="dues" className="form-label">Cuotas</label>
-                      <input
-                          type="number"
-                          name="dues"
-                          className="form-control"
-                          placeholder="12"
-                          min="1"
-                          value={this.state.contract.dues}
-                          onChange={this.updateValueFee}
-                      >
-                      </input>
+                  <input
+                    type="number"
+                    name="dues"
+                    className="form-control"
+                    placeholder="12"
+                    min="1"
+                    step="1"
+                    value={this.state.contract.dues}
+                    onChange={this.updateValueFee}
+                    required
+                  >
+                  </input>
                 </div>
 
-                <div className="mb-3">
+                <div className="col-md-6">
                   <label htmlFor="value_fee" className="form-label">Valor Cuota</label>
-                      <input
-                          type="number"
-                          name="value_fee"
-                          className="form-control"
-                          step="any"
-                          placeholder="10.000,00"
-                          min="0"
-                          value={this.state.contract.value_fee}
-                          onChange={this.onChange}>
-                      </input>
+                    <input
+                      type="number"
+                      name="value_fee"
+                      className="form-control"
+                      step="0.01"
+                      placeholder="10.000,00"
+                      min="0"
+                      value={this.state.contract.value_fee}
+                      onChange={this.onChange}
+                      required
+                    >
+                    </input>
                 </div>
 
-                <div className="mb-3">
-                  <label htmlFor="grace_month" className="form-label">Mes de Gracia</label>
-                      <input
-                          type="checkbox"
-                          name="grace_month"
-                          className="checkbox"
-                          value={this.state.contract.grace_month}
-                          onChange={this.onChange}
-                      >
-                      </input>
+                <div className="col-md-3">
+                  <div className="form-check form-switch">
+                    <input
+                      type="checkbox"
+                      name="grace_month"
+                      className="form-check-input"
+                      value={this.state.contract.grace_month}
+                      onChange={this.onChange}
+                    >
+                    </input>
+                    <label htmlFor="grace_month" className="form-check-label">Mes de Gracia</label>
+                  </div>
                 </div>
 
-                <div className="mb-3">
+                <div className="col-md-3">
                   <label htmlFor="payday" className="form-label">Día de Pago</label>
-                      <input
-                          type="date"
-                          name="payday"
-                          className="form-control"
-                          step="any"
-                          placeholder="10.000,00"
-                          onChange={this.onChange}>
-                      </input>
+                  <input
+                    type="date"
+                    name="payday"
+                    className="form-control"
+                    step="any"
+                    placeholder="10.000,00"
+                    onChange={this.onChange}
+                    required
+                  >
+                  </input>
                 </div>
 
-                <button type="submit" className="btn btn-primary">
-                  Crear Contrato
-                </button>
+                <div className="col-12 mb-3">
+                  <button type="submit" className="btn btn-primary me-2">
+                    Crear Contrato
+                  </button>
 
-                <Link type="button" to="/contracts" className="btn btn-secondary">
-                  Volver
-                </Link>
+                  <Link type="button" to="/contracts" className="btn btn-secondary">
+                    Volver
+                  </Link>
+                </div>
               </form>
             </>
         );
