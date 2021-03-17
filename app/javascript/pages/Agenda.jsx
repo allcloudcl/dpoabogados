d4bf245b50c7842648af8794885265f58d098587
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-03-17 08:55:42 -0400

    Move tui.calendar css from react to assets

--- app/javascript/pages/Agenda.jsx
+++ app/javascript/pages/Agenda.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Calendar from '@toast-ui/react-calendar';
-import 'tui-calendar/dist/tui-calendar.css';
-
-import 'tui-date-picker/dist/tui-date-picker.css';
-import 'tui-time-picker/dist/tui-time-picker.css';
 
 class Agenda extends React.Component {
 
