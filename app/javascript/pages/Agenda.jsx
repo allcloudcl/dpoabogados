21d9f8c71c18b3390af9113f076619ecf550b589
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-03-17 17:47:07 -0400

    Start customizing Calendar

--- app/javascript/pages/Agenda.jsx
+++ app/javascript/pages/Agenda.jsx
@@ -9,12 +9,32 @@ class Agenda extends React.Component {
     }
 
     render() {
+        let weekOptions = {
+            startDayOfWeek: 1,
+            daynames: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+            hourStart: 7,
+            hourEnd: 24
+        }
+
+        let monthOptions = {
+            daynames: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+            startDayOfWeek: 1,
+        }
+
         return (
             <>
               <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 className="h2">Agenda</h1>
               </div>
-              <Calendar usageStatistics={false} />
+              <Calendar
+                usageStatistics={false}
+                month={monthOptions}
+                week={weekOptions}
+                taskView={false}
+                scheduleView={['time']}
+                useCreationPopup={true}
+                useDetailPopup={true}
+              />
             </>
         );
     }
