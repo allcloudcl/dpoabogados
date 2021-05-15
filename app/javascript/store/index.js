77515703d73121a4393763477bfdb5969504e889
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2021-05-15 12:06:10 -0400

    Remove redux-devtools-extension

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 
 const middleware = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
